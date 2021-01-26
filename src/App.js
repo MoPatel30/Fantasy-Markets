0a558aa12da5158a02ae22b25ad395f8baa96741
Mohammed Patel <mopatel1214@gmail.com>
2021-01-26 16:55:16 -0600

    removed some files, setup basic components/files to learn the basics of react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import PracticeCounter from "./PracticeCounter/PracticeCounter.jsx"
 
-function App() {
+
+
+function App({name}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "App">
+      <h1>Hello {name}</h1>
+      <PracticeCounter />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
